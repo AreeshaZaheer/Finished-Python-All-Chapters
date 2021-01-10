34f7540cb323f0ef8dd5a7d4eae8d1809f6c4f4f
AreeshaZaheer <77195322+AreeshaZaheer@users.noreply.github.com>
2021-01-10 13:32:47 +0500

    Add files via upload

---
+++ 05_01_begin.py
@@ -0,0 +1,13 @@
+print("~~~ The Shimmy ~~~")
+
+print("Take one step to the right and stomp.")
+print("Take one step to the left and stomp.")
+print("Shake those hips!") 
+
+print("Take one step to the right and stomp.")
+print("Take one step to the left and stomp.")
+print("Shake those hips!") 
+
+print("Take one step to the right and stomp.")
+print("Take one step to the left and stomp.")
+print("Shake those hips!") 
---
+++ 05_07.py
@@ -0,0 +1,7 @@
+# Prints out the name of a favorite city
+def favorite_city(Lahore):
+    print("One of my favorite cities is", Lahore)
+
+favorite_city("Lahore")
+favorite_city("Lahore")
+favorite_city("Lahore")
